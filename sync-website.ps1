1f4cb369b55f65493035a4da60b1cf21c5ad7295
Leo520-dev <leo520-dev@users.noreply.github.com>
2026-07-04 10:44:21 +0000

    auto sync

---
+++ sync-website.ps1
@@ -0,0 +1,8 @@
+﻿$vault = 'D:\Obsidian Vault'
+$site = 'D:\quartz-site\content'
+Copy-Item "$vault\*" $site -Recurse -Force
+cd D:\quartz-site
+git add -A
+git commit -m "auto sync" --allow-empty
+git push
+Write-Host "✅ 网站已更新！ https://Leo520-dev.github.io/quartz/"
